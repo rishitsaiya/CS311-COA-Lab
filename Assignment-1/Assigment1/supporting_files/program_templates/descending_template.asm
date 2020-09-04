55fe4bcafacd6f04189bf682eacce25a83e3b383
rishitsaiya <rishitsaiya@gmail.com>
2020-09-04 17:55:03 +0530

    Assignment-1 PS & Supporting Files added

---
+++ Assignment-1/Assigment1/supporting_files/program_templates/descending_template.asm
@@ -0,0 +1,16 @@
+	.data
+a:
+	70
+	80
+	40
+	20
+	10
+	30
+	50
+	60
+n:
+	8
+	.text
+// your code here
+// you may change the numbers in the array, and the size of the array; but allow the name of the array to remain as 'a', and size as 'n'
+// remove these comments!
---
+++ Assignment-1/Assigment1/supporting_files/program_templates/palindrome_template.asm
@@ -0,0 +1,7 @@
+	.data
+a:
+	10
+	.text
+// your code here to check if 'a' is a palindrome
+// you may change the value of 'a'
+// remove these comments!
