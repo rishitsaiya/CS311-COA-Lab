a171cb9250c73a9a7eb94d06e087cbf54ffd32a9
Rishit Saiya <180010027@iitdh.ac.in>
2020-09-14 22:36:47 +0530

    Update palindrome_template.asm

--- Assignment-1/Assigment1/supporting_files/palindrome_template.asm
+++ Assignment-1/Assigment1/supporting_files/palindrome_template.asm
@@ -8,16 +8,16 @@ main:
 	add %0, %x0, %x5			;0 + 0 = x5
 Division:
 	divi %x4, 10, %x4			;x4 / 10 = x4 (To get remaining digits except last)
-	beq %x4, %x0, EndPalindrome	;if(x4 == 0), then go to EndPalindrome fn
+	beq %x4, %x0, endPalindrome	;if(x4 == 0), then go to EndPalindrome fn
 	add %x31, %x5, %x5			; x5 not NULL
 	muli %x5, 10, %x5			; x5 * 10 = x5
 	jmp Division
-EndPalindrome:
+endPalindrome:
 	add %x31, %x5, %x5			;
-	beq %x3, %x5, Palindrome	;if(x3 == x5), then go to Palindrome
-NotPalindrome:
+	beq %x3, %x5, isPalindrome	;if(x3 == x5), then go to Palindrome
+notPalindrome:
 	subi %x0, 1, %x10			;x10 = -1 (a is not Palindrome)
 	end
-Palindrome:
+isPalindrome:
 	addi %x0, 1, %x10			;x10 = 1 (a is Palindrome)
 	end
