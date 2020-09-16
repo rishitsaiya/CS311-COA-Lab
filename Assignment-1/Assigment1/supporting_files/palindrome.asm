ea911455eb0e96946c6cd28d3dc628e1e3d4a798
abhiisshheekk <abhiisshheekk@gmail.com>
2020-09-16 11:51:54 +0530

    another palindrome update

--- Assignment-1/Assigment1/supporting_files/palindrome.asm
+++ Assignment-1/Assigment1/supporting_files/palindrome.asm
@@ -1,23 +1,28 @@
 	.data
 a:
-	2002
+	0
 	.text
 main:
 	load %x0, $a, %x3			; x3 = a
 	addi %x3, 0, %x4			; x4 = a
 	addi %x0, 0, %x5			; x5 = 0
+	addi %x0, 10, %x17			; x17 = 10
+	blt %x3, %x0, isNotPalindrome			; if (x3 < x0), then goto isNotPalindrome (for negative numbers)
+	blt %x3, %x17, isPalindrome			; if (x3 < x17), then goto isPalindrome (for single digit numbers)
 loop:
-	divi %x4, 10, %x4			; x4 = x4 / 10 (To get remaining digits except last)
+	div %x4, %x17, %x4			; x4 = x4 / 10 (To get remaining digits except last)
 	beq %x4, %x0, endLoop			; if (x4 == 0), then goto endLoop fn
 	add %x31, %x5, %x5			; x5 = x5 + x31 (add remainder of division)
-	muli %x5, 10, %x5			; x5 = x5 * 10 (Reversing the digits of a)
+	mul %x5, %x17, %x5			; x5 = x5 * 10 (Reversing the digits of a)
 	jmp loop
 endLoop:
 	add %x31, %x5, %x5			; x5 = x5 + x31 (add remainder of division)
 	beq %x3, %x5, isPalindrome			;if (x3 == x5), then go to Palindrome
 isNotPalindrome:
-	subi %x0, 1, %x10			; x10 = -1 ('a' is not a Palindrome)
+	subi %x0, 0, %x10			; x10 = x0 - 0
+	subi %x10, 1, %x10			; x10 = -1 ('a' is not a Palindrome)
 	end
 isPalindrome:
-	addi %x0, 1, %x10			; x10 = 1 ('a' is a Palindrome)
+	subi %x0, 0, %x10			; x10 = x0 - 0
+	addi %x10, 1, %x10			; x10 = 1 ('a' is a Palindrome)
 	end
