ad25b9b4984459257a6cb46dd841042d3089342c
harshraj22 <harshraj22aug@gmail.com>
2020-09-23 14:24:14 +0530

    added more structored code

--- Assignment-2/Assignment2/CS311_assignment2/src/generic/Simulator.java
+++ Assignment-2/Assignment2/CS311_assignment2/src/generic/Simulator.java
@@ -3,11 +3,25 @@ package generic;
 import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+import org.graalvm.compiler.asm.aarch64.AArch64Assembler.Instruction;
+
+// import org.graalvm.compiler.asm.aarch64.AArch64Assembler.Instruction;
 
 
 public class Simulator {
 
 	static FileInputStream inputcodeStream = null;
+	public static Map<Instruction.OperationType, String> mapping = new HashMap<>();
+
+	public Simulator() {
+		mapping.put(Instruction.OperationType.add, "00000");
+		mapping.put(Instruction.OperationType.addi, "00001");
+
+	}
 
 	public static void setupSimulation(String assemblyProgramFile) {
 		int firstCodeAddress = ParsedProgram.parseDataSection(assemblyProgramFile);
@@ -15,10 +29,31 @@ public class Simulator {
 		ParsedProgram.printState();
 	}
 
+	private static String toBinaryString(int n) {
+		// Remove this conditional statement
+		if (n >= 0) return String.valueOf(n);
+
+		Stack<Integer> bits = new Stack<>();
+		do {
+			bits.push(n % 2);
+			n /= 2;
+		} while (n != 0);
+
+		StringBuilder builder = new StringBuilder();
+		while (!bits.isEmpty()) {
+			builder.append(bits.pop());
+		}
+		return builder.toString();
+	}
+
 	private static String convert(Operand inst) {
+		if (inst.getOperandType() == Operand.OperandType.Label)
+			return toBinaryString(ParsedProgram.symtab.get(inst.getLabelValue()));
+
 		// write logic for converting to binary/ hex
+		return toBinaryString(inst.getValue());
 		// check if inst is a label, in that case, use its value 
-		return String.valueOf(inst.getValue());
+		// return String.valueOf(inst.getValue());
 	}
 
 	public static void assemble(String objectProgramFile) {
@@ -44,12 +79,14 @@ public class Simulator {
 				 * in case it is a label, it would call ParsedProgram.symtab.get()
 				 * to get the address corresponding to the label
 				 */
+				// print operation type, use toBinaryString() instead of convert()
+
 				if (inst.getSourceOperand1() != null)
-					file.write(String.valueOf((inst.getSourceOperand1().getValue())));
+					file.write(convert(inst.getSourceOperand1()));
 				if (inst.getSourceOperand2() != null)
-					file.write(String.valueOf((inst.getSourceOperand2().getValue())));
+					file.write(convert(inst.getSourceOperand2()));
 				if (inst.getDestinationOperand() != null)
-					file.write(String.valueOf(inst.getDestinationOperand().getValue()));
+					file.write(convert(inst.getDestinationOperand()));
 				file.write(inst.toString());
 			}
 
