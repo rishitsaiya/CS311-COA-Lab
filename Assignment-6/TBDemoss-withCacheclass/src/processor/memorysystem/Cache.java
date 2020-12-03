de2dfc093513e3adad98e06c9f8e1709d3db8ae5
harshraj22 <harshraj22aug@gmail.com>
2020-12-03 11:57:32 +0530

    first set of changes

--- Assignment-6/TBDemoss-withCacheclass/src/processor/memorysystem/Cache.java
+++ Assignment-6/TBDemoss-withCacheclass/src/processor/memorysystem/Cache.java
@@ -15,14 +15,15 @@ public class Cache implements Element {
     boolean isPresent = true;
     int temp;
     int[] index;
+
 	public Cache(Processor containingProcessor, int l, int s) {
 		this.containingProcessor = containingProcessor;
         this.latency = l;
         this.csize = s;
-        temp   = (int)(Math.log(this.csize/8)/Math.log(2));
-        cach=new CacheLine[csize/8];
-		for(int i=0;i<csize/8;i++) {
-			cach[i]=new CacheLine();
+        temp = (int)(Math.log(this.csize/8)/Math.log(2));
+        cach = new CacheLine[csize/8];
+		for(int i = 0; i < csize/8; i++) {
+			cach[i] = new CacheLine();
 		}
     }
     
@@ -40,31 +41,32 @@ public class Cache implements Element {
 
     
     public int cacheRead(int address){
-        String a=Integer.toBinaryString(address);
-        String ind ="";
+        String a = Integer.toBinaryString(address);
+        String ind = "";
         int temp_ind;
        
-        for(int i =0;i<32-a.length();i++){
+        for(int i = 0; i < 32-a.length(); i++){
             a = "0" + a;
         }
-        for(int i = 0; i<temp;i++ ){
+        for(int i = 0; i < temp; i++ ) {
             ind = ind + "1";
         }
-        if(temp ==0){
+        if(temp == 0){
             temp_ind = 0;
         }
-        else
-          {
-               temp_ind = address & Integer.parseInt(ind, 2);
-          }
-        System.out.println("in the Cache " +address);
+        else {
+            temp_ind = address & Integer.parseInt(ind, 2);
+        }
+
+        System.out.println("in the Cache " + address);
         int add_tag = Integer.parseInt(a.substring(0, a.length()-temp),2);
-        if(add_tag ==cach[temp_ind].tag[0]){
+
+        if(add_tag == cach[temp_ind].tag[0]){
             cach[temp_ind].lru = 1;
             isPresent = true;
             return cach[temp_ind].data[0];
         }
-        else if(add_tag ==cach[temp_ind].tag[1]){
+        else if(add_tag == cach[temp_ind].tag[1]){
             cach[temp_ind].lru = 0;
             isPresent = true;
             return cach[temp_ind].data[1];
@@ -78,24 +80,23 @@ public class Cache implements Element {
     }
 
     public void cacheWrite(int address, int value){
-        String a=Integer.toBinaryString(address);
-        String ind ="";
+        String a = Integer.toBinaryString(address);
+        String ind = "";
         int temp_ind;
 
-        for(int i =0;i<32-a.length();i++){
+        for(int i = 0;i <32-a.length(); i++){
             a = "0" + a;
         }
-        for(int i = 0; i<temp;i++ ){
+        for(int i = 0; i < temp; i++ ){
             ind = ind + "1";
         }
-        if(temp ==0){
+        if(temp == 0){
             temp_ind = 0;
         }
-        else
-          {
-               temp_ind = address & Integer.parseInt(ind, 2);
-          }
-        // int temp_ind = address & Integer.parseInt(ind, 2);
+        else {
+            temp_ind = address & Integer.parseInt(ind, 2);
+        }
+
         int tag = Integer.parseInt(a.substring(0, a.length()-temp),2);
         cach[temp_ind].setValue(tag, value);
 
@@ -121,7 +122,7 @@ public class Cache implements Element {
             else{
                 System.out.println("Missed");
                 this.miss_addr = ee.getAddressToReadFrom();
-                ee.setEventTime(Clock.getCurrentTime()+Configuration.mainMemoryLatency+1);
+                ee.setEventTime(Clock.getCurrentTime() + Configuration.mainMemoryLatency+1);
                 Simulator.getEventQueue().addEvent(ee);
                 handleCacheMiss(ee.getAddressToReadFrom());
             }
@@ -129,40 +130,24 @@ public class Cache implements Element {
        else if(e.getEventType() == Event.EventType.MemoryResponse){
             MemoryResponseEvent ee = (MemoryResponseEvent) e;
             cacheWrite(this.miss_addr, ee.getValue());
-            // Simulator.getEventQueue().addEvent(
-            //     new MemoryResponseEvent(
-            //         Clock.getCurrentTime() + 1, 
-            //         this, 
-            //         ee.getRequestingElement(), 
-            //         data)
-            // );
+            
         }
         
         else if(e.getEventType() == Event.EventType.MemoryWrite){
             System.out.println("handle event cache memory write");
             MemoryWriteEvent ee = (MemoryWriteEvent) e;
             cacheWrite(ee.getAddressToWriteTo(), ee.getValue());
-            // int data = cacheRead(ee.getAddressToWriteTo());
+
             containingProcessor.getMainMemory().setWord(ee.getAddressToWriteTo(), ee.getValue());
-            // if(true) {
-            //     cacheWrite(ee.getAddressToWriteTo(), ee.getValue());
-            // }
+
             Simulator.getEventQueue().addEvent(
 				new ExecutionCompleteEvent(
 					Clock.getCurrentTime()+Configuration.mainMemoryLatency, 
 					containingProcessor.getMainMemory(), 
 					ee.getRequestingElement())
 			);
-            // cacheWrite(this.miss_addr, ee.getValue());
-            // Simulator.getEventQueue().addEvent(
-            //     new MemoryResponseEvent(
-            //         Clock.getCurrentTime() + 1, 
-            //         this, 
-            //         ee.getRequestingElement(), 
-            //         data)
-            // );
+            
 		}
-		
 		
 	}
 
--- Assignment-6/TBDemoss-withCacheclass/src/processor/memorysystem/CacheLine.java
+++ Assignment-6/TBDemoss-withCacheclass/src/processor/memorysystem/CacheLine.java
@@ -5,17 +5,37 @@ import processor.*;
 
 public class CacheLine{
     int[] tag = new int[2]; 
-    // int index;
     int[] data = new int[2];
     int lru;
 
-    public CacheLine()
-	{   
-        // this.index = index;
+    public CacheLine() {
         this.tag[0] = -1;
         this.tag[1] = -1;
-        this.lru =0;
+        this.lru = 0;
     }
+
+    public CacheLine(int newLru) {
+        this.lru = newLru;
+        this.tag[0] = -1;
+        this.tag[1] = -1;
+    }
+
+    public int getData(int index) {
+        return this.data[index];
+    }
+
+    public int getTag(int index) {
+        return this.tag[index];
+    }
+
+    public int getLru() {
+        return this.lru;
+    }
+
+    public int setLru(int newLru) {
+        this.lru = newLru;
+    }
+
     public void setValue(int tag, int value) {
         if(tag == this.tag[0]) {
             this.data[0] = value;
@@ -25,11 +45,14 @@ public class CacheLine{
             this.data[1] = value;
             this.lru = 0;
         }
-
         else {
             this.tag[this.lru] = tag;
             this.data[this.lru] = value;
             this.lru = 1- this.lru;
         }
 	}
+
+    public String toString() {
+        return this.lru;
+    }
 }
